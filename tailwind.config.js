37200903a7108389ec8cbb915bc31933469fb94b
Nick Sohl <90460746+nick-sohl@users.noreply.github.com>
2023-09-12 22:38:22 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms'),
+    require("daisyui"),
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwindcss-gradients'),
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
